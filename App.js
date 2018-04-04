dd21c9c106b3578e8dc517b2b4183b34764588bd
Ashish Pandey <ashishpandeyone@gmail.com>
2018-04-04 17:06:59 +0530

    single file player using react-native-track-player

--- App.js
+++ App.js
@@ -1,58 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
+  Button,
   Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+import TrackPlayer from 'react-native-track-player';
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(Props) {
+    super(Props);
+    this.state = {
+      isPlaying: false,
+    };
+  }
+
+  onPlayStopButtonPress = () => {
+    this.setState({
+      isPlaying: !this.state.isPlaying
+    }, () => {
+      if (this.state.isPlaying) {
+        this.onSSPlay();
+      } else {
+        this.onSSStop();
+      }
+    });
+  }
+
+  onSSPlay = async () => {
+    await TrackPlayer.setupPlayer();
+    await TrackPlayer.add({
+        id: '1',
+        url: require('./android/app/src/main/res/raw/hdfc_d_s_2_1.mp3'),
+        title: '1',
+        artist: '1'
+      });
+    await TrackPlayer.play();
+  };
+
+  onSSStop = async () => {
+    await TrackPlayer.stop();
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View>
+        <Button
+          title={this.state.isPlaying ? 'Stop' : 'Play'}
+          onPress={this.onPlayStopButtonPress}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
